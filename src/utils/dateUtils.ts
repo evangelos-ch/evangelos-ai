b45a4648fcc0a7582a5f5590d00c8e8b10def9cd
evangelos-ch <angelos.h@hotmail.com>
2023-11-14 15:54:10 +0000

    Blog index, no navbar on scroll yet

---
+++ src/utils/dateUtils.ts
@@ -0,0 +1,63 @@
+import type { MarkdownInstance } from 'astro'
+
+export const relativeDate = (date: string) => {
+  const postDate = new Date(date)
+  const currentDate = new Date()
+  const diff = (currentDate.getTime() - postDate.getTime()) / 1000 // in seconds
+  const week = 60 * 60 * 24 * 7
+  const month = 60 * 60 * 24 * 30
+
+  if (diff < week) {
+    return `${Math.floor(diff / (week / 7))} days ago`
+  }
+
+  if (diff < month) {
+    const weeksAgo = Math.floor(diff / (month / 4))
+    return `${weeksAgo} week${weeksAgo > 1 ? 's' : ''} ago`
+  }
+
+  return postDate.toLocaleDateString('en-GB')
+}
+
+export type Post = MarkdownInstance<Record<string, any>>
+export interface PostMap {
+  [year: number]: {
+    [month: number]: Post[]
+  }
+}
+export const MonthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
+
+export const splitPostsByDate = (posts: Post[]) => {
+  let ret: PostMap = {}
+
+  posts.forEach((post) => {
+    const date = new Date(post.frontmatter.pubDate)
+    const year = date.getFullYear()
+    const month = date.getMonth()
+
+    if (!ret[year]) {
+      ret[year] = {}
+    }
+
+    if (!ret[year][month]) {
+      ret[year][month] = []
+    }
+
+    ret[year][month].push(post)
+  })
+
+  return ret
+}
