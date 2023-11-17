0cbcec088e6815eae4638280c73c245d7c7300e4
evangelos-ch <angelos.h@hotmail.com>
2023-11-17 04:04:26 +0000

    LaTeX Math support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
-
 import prefetch from '@astrojs/prefetch'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://evangelos.ai',
-  integrations: [sitemap(), prefetch()]
+  integrations: [sitemap(), prefetch()],
+  markdown: {
+    remarkPlugins: ['remark-math'],
+    rehypePlugins: [
+      [
+        'rehype-katex',
+        {
+          // Katex plugin options
+        }
+      ]
+    ]
+  }
 })
